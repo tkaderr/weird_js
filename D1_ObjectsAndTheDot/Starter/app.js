50cb36288cf0d7ce4142e7807e7feab0fb415020
Tahim Kader <tahim.kader@gmail.com>
2017-08-29 14:50:27 -0700

    review

---
+++ D1_ObjectsAndTheDot/Starter/app.js
@@ -0,0 +1,10 @@
+var person = new Object();
+
+//primitive property
+//creates memory and address
+person["firstname"] = "Tony";
+
+person.address = new Object();
+person.address.street = "11 Main St.";
+
+console.log(person);
